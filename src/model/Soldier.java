6a780fa998f5d7f41e8d722357be4ea48fb9da1b
Adam Baszczynski <acbaszczynski@dmacc.edu>
2019-10-09 16:36:18 -0500

    Filled models
    Filled soldier and loaditems models

--- src/model/Soldier.java
+++ src/model/Soldier.java
@@ -21,4 +21,37 @@ public class Soldier {
 	private String name;
 	@Column(name="BIRTH_DATE")
 	private LocalDate birthdate;
+	
+	public Soldier() {
+		super();
+	}
+	
+	public Soldier(String name, LocalDate birthdate) {
+		this.name = name;
+		this.birthdate = birthdate;
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public LocalDate getBirthdate() {
+		return birthdate;
+	}
+	public void setBirthdate(LocalDate birthdate) {
+		this.birthdate = birthdate;
+	}
+
+	@Override
+	public String toString() {
+		return "Soldier [id=" + id + ", name=" + name + ", birthdate=" + birthdate + "]";
+	}
 }
