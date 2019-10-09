6c85db63397e107bdf6273c2380ae1d01bd9f27e
Mooneruma <31321473+Mooneruma@users.noreply.github.com>
2019-10-09 16:37:00 -0500

    Loadouthelper
    made and edited loadoutHelper/Loadout/and the Index is now created.

--- src/controller/LoadoutHelper.java
+++ src/controller/LoadoutHelper.java
@@ -1,5 +1,42 @@
 package controller;
 
-public class LoadoutHelper {
+import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+import model.Loadout;
+
+public class LoadoutHelper {
+	static EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("SoldierLoadoutMenu");
+
+	public void insertLoadout(Loadout l) {
+		EntityManager em = emfactory.createEntityManager();
+		em.getTransaction().begin();
+		em.persist(l);
+		em.getTransaction().commit();
+		em.close();
+	}
+
+	public List<Loadout> showAllLoadout() {
+		EntityManager em = emfactory.createEntityManager();
+		List<Loadout> allLoadouts = em.createQuery("SELECT l FROM Loadout l").getResultList();
+		return allLoadout;
+	}
+
+	public Loadout searchForLoadoutByName(String LoadoutName) {
+		// TODO Auto-generated method stub
+		EntityManager em = emfactory.createEntityManager();
+		em.getTransaction().begin();
+		TypedQuery<Loadout> typedQuery = em.createQuery("select l from Loadout l where l.LoadoutName = :selectedName",
+				Loadout.class);
+		typedQuery.setParameter("selectedName", LoadoutName);
+		typedQuery.setMaxResults(1);
+
+		Loadout found = typedQuery.getSingleResult();
+		em.close();
+		return found;
+	}
 }
--- src/model/Loadout.java
+++ src/model/Loadout.java
@@ -1,5 +1,62 @@
 package model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "loadout")
 public class Loadout {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "loadout_ID")
+	private int loudout_id;
+	@Column(name = "loudout_name")
+	private String loudout_name;
+	@Column(name = "soldier_id")
+	private String soldier_id;
+
+	public Loadout() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Loadout(String loudout_name, int loudout_id) {
+		super();
+		this.loudout_name = loudout_name;
+		this.loudout_id = loudout_id;
+	}
+
+	public int getLoudout_id() {
+		return loudout_id;
+	}
+
+	public void setLoudout_id(int loudout_id) {
+		this.loudout_id = loudout_id;
+	}
+
+	public String getLoudout_name() {
+		return loudout_name;
+	}
+
+	public void setLoudout_name(String loudout_name) {
+		this.loudout_name = loudout_name;
+	}
+
+	public String getSoldier_id() {
+		return soldier_id;
+	}
+
+	public void setSoldier_id(String soldier_id) {
+		this.soldier_id = soldier_id;
+	}
+
+	@Override
+	public String toString() {
+		return "Loadout [id=" + loudout_id + ", store=" + loudout_name + ", item=" + soldier_id + "]";
+	}
 
 }
