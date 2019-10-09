a3ae475fafa84178ac2607b1b0fcfbac269cc583
Mooneruma <31321473+Mooneruma@users.noreply.github.com>
2019-10-09 17:49:31 -0500

    deleteStatements
    added delets to the navigation servlet

--- src/controller/NavigationServlet.java
+++ src/controller/NavigationServlet.java
@@ -1,11 +1,14 @@
 package controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import model.ListItem;
 
 /**
  * Servlet implementation class NavigationServlet
@@ -13,43 +16,78 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/NavigationServlet")
 public class NavigationServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public NavigationServlet() {
-        super();
-    }
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public NavigationServlet() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		SoldierHelper sh = new SoldierHelper();
 		LoadoutHelper lh = new LoadoutHelper();
 		LoadItemsHelper lih = new LoadItemsHelper();
 		String act = request.getParameter("doThis");
-		
+
 		if (act == null) {
-			
+
 		} else if (act.equals("addSoldier")) {
-			
+
 		} else if (act.equals("editSoldier")) {
-			
+
 		} else if (act.equals("deleteSoldier")) {
-			
+
+			try {
+				Integer Item_Id = Integer.parseInt(request.getParameter("id"));
+				ListItem itemToDelete = lih.searchForItemById(Item_Id);
+				lih.deleteItem(itemToDelete);
+
+			} catch (NumberFormatException e) {
+				System.out.println("Forgot to click a button");
+			} finally {
+				getServletContext().getRequestDispatcher("/viewAllItemsServlet").forward(request, response);
+			}
+
 		} else if (act.equals("addLoadout")) {
-			
+
 		} else if (act.equals("editLoadout")) {
-			
+
 		} else if (act.equals("deleteLoadout")) {
-			
+
+			try {
+				Integer Item_Id = Integer.parseInt(request.getParameter("id"));
+				ListItem itemToDelete = lih.searchForItemById(Item_Id);
+				lih.deleteItem(itemToDelete);
+
+			} catch (NumberFormatException e) {
+				System.out.println("Forgot to click a button");
+			} finally {
+				getServletContext().getRequestDispatcher("/viewAllItemsServlet").forward(request, response);
+			}
+
 		} else if (act.equals("addItem")) {
-			
+
 		} else if (act.equals("editItem")) {
-			
+
 		} else if (act.equals("deleteItem")) {
-			
+
+			try {
+				Integer Item_Id = Integer.parseInt(request.getParameter("id"));
+				ListItem itemToDelete = lih.searchForItemById(Item_Id);
+				lih.deleteItem(itemToDelete);
+
+			} catch (NumberFormatException e) {
+				System.out.println("Forgot to click a button");
+			} finally {
+				getServletContext().getRequestDispatcher("/viewAllItemsServlet").forward(request, response);
+			}
+
 		}
 	}
 
