63a2f1cc499a639f5c460dcdf9907ca691b3317e
Adam Baszczynski <acbaszczynski@dmacc.edu>
2019-10-09 15:34:24 -0500

    Packages filled with classes

---
+++ src/model/Soldier.java
@@ -0,0 +1,24 @@
+package model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="soldier")
+public class Soldier {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="ID")
+	private int id;
+	@Column(name="NAME")
+	private String name;
+	@Column(name="BIRTH_DATE")
+	private LocalDate birthdate;
+}
